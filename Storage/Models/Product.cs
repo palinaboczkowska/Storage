85251c975630f1257a836a4a60168b75e3453b14
Palina Boczkowska <palina.boczkowska@gmail.com>
2025-12-10 14:33:50 +0100

    Add  data annotations

--- Storage/Models/Product.cs
+++ Storage/Models/Product.cs
@@ -7,18 +7,25 @@ namespace Storage.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
         public string Name { get; set; } = string.Empty;
 
+        [Range(1, 100000)]
         public int Price { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime OrderDate { get; set; }
 
+        [Required]
         public string Category { get; set; }
 
         public string Shelf { get; set; }
 
+        [Range(0, 1000)]
         public int Count { get; set; }
 
+        [StringLength(500)]
         public string Description { get; set; }
 
     }
